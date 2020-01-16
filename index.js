400e61f75018f79d8b31d5a6d2c751f7fccd0ef6
Hedde van Heerde <heddevanheerde4@gmail.com>
2020-01-16 22:31:00 +0100

    Add total_godpower to >level, removed gold there, addes base for >gold command, added last_username stat for users.

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ client.on('ready', () => {
     if (totalGodpower === undefined) {
         totalGodpower = 0;
     }
+    client.channels.get(levelup_channel).send('Succesfully restarted!');
 });
 
 client.on('message', message => {
@@ -60,6 +61,9 @@ client.on('message', message => {
             if (command_channels.includes(message.channel.id)) {
                 if (message.content.toLowerCase().startsWith(`${prefix}level`)) {
                     displayLevel(message);
+                }
+                if (message.content.toLowerCase().startsWith(`${prefix}gold`)) {
+                    displayGold(message);
                 }
             }
         }
@@ -88,10 +92,12 @@ async function giveGodpower(message) {
     let godpowerAdd = Math.floor(Math.random() * 5) + 3;
     if(userDoc.data()[message.author.id] === undefined) {
         User[message.author.id] = {
+            last_username: string,
             godpower: 0,
             total_godpower: 0,
             level: 0
         }
+        User[message.author.id].last_username = user.username+'#'+user.discriminator;
         await userData.set(User, {merge: true});
     } else {
         User[message.author.id] = userDoc.data()[message.author.id];
@@ -118,13 +124,14 @@ async function giveGodpower(message) {
         let lvlUpEmbed = new Discord.RichEmbed()
             .setColor('d604cf')
             .setTitle(nickname+' levelled UP! <:screen_pantheonup:441043802325778442>')
-            .setDescription('You gathered '+nextLevel+' godpower <:stat_godpower:401412765232660492> and levelled up to level '+User[message.author.id].level+'! :tada:')
+            .setDescription('You gathered '+nextLevel+' godpower <:stat_godpower:401412765232660492> and levelled up to level '+User[message.author.id].level+'! :tada: - You now have '+User[message.author.id].total_godpower+' godpower total.')
             .addField("Gold rewarded", 'This isn\'t implemented yet :/')
             .setFooter(`You'll need ${newNextLevel} godpower for level ${newLevel}.`, message.author.displayAvatarURL);
         client.channels.get(levelup_channel).send("Congratulations on reaching level "+User[message.author.id].level+', '+message.author+"!");
         client.channels.get(levelup_channel).send(lvlUpEmbed);
     }
 
+    User[message.author.id].last_username = user.username+'#'+user.discriminator;
     userData.set(User, {merge: true});
     userData.update({1:totalGodpower});
 
@@ -169,11 +176,15 @@ async function displayLevel(message) {
     .setColor('32cd32')
     .addField("Level", curLevel, true)
     .addField("Godpower", curGodpower, true)
-    .addField("Rank", 'This hasn\'t been added yet.', false)
-    .addField("Gold", 'This hasn\'t been added yet.', true)
+    .addField("Total godpower", User[user.id].total_godpower, false)
+    .addField("Rank", 'This hasn\'t been added yet.', true)
     .setFooter(`${difference} godpower needed for level ${nextLevel}.`, user.displayAvatarURL);
 
     message.channel.send(lvlEmbed);
 }
 
 client.login(token)
+
+async function displayGold(message) {
+    message.reply('this hasn\'t been implemented yet :/');
+}
