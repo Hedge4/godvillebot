e81d51e7b7f4d10cd3d92334e5507085591474f6
Hedde van Heerde <heddevanheerde4@gmail.com>
2020-01-16 07:34:03 +0100

    Switched from storing data locally to storing it on a Firebase server

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 const Discord = require('discord.js');
 const { prefix, token, owner, server, bot_id, no_xp_channels, levelup_channel, command_channels, no_xp_prefixes, cdSeconds } = require('./config.json');
 const client = new Discord.Client();
-const fs = require('fs');
-let godpower = require('./godpower.json');
 let godpowerCooldown = new Set();
+
+const admin = require('firebase-admin')
+let serviceAccount = require('./serviceAccountKey.json')
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+let db = admin.firestore();
+const userData = db.collection('data').doc('users');
+let totalGodpower = 0;
+let getDoc = userData.get()
+    .then (doc => {
+        totalGodpower = doc.data()[1]}
+    );
+
+/*db.collection('data').get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+    });
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
+
+async function test() {
+    let a = await userData.get();
+    let b = a.data();
+    console.log(b);
+}
+test()*/
 
 client.on('ready', () => {
     const currentDate = new Date();
     console.log('\n' + currentDate + ` - Logged in as ${client.user.tag}!`);
     console.log('Logged in to the following guilds: ' + client.guilds.array().sort() + '\n');
     client.user.setActivity(`${prefix}level` + ' | Still testing! - by Wawajabba');
-    if(!godpower[1]) {
-        godpower[1] = {
-            godpower: 0,
-        };
+    if (totalGodpower === undefined) {
+        totalGodpower = 0;
     }
 });
 
@@ -40,7 +66,7 @@ client.on('message', message => {
     }
 });
 
-function giveGodpower(message) {
+async function giveGodpower(message) {
     let spam = 0;
 
     no_xp_prefixes.forEach(element => {
@@ -57,29 +83,34 @@ function giveGodpower(message) {
         return;
     }
 
+    let userDoc = await userData.get();
+    let User = {};
     let godpowerAdd = Math.floor(Math.random() * 5) + 3;
-    if(!godpower[message.author.id]) {
-        godpower[message.author.id] = {
+    if(userDoc.data()[message.author.id] === undefined) {
+        User[message.author.id] = {
             godpower: 0,
             total_godpower: 0,
             level: 0
-        };
+        }
+        await userData.set(User, {merge: true});
+    } else {
+        User[message.author.id] = userDoc.data()[message.author.id];
     }
 
     console.log(godpowerAdd+' godpower added for user '+message.author.username+' in channel '+message.channel.name)
     godpowerCooldown.add(message.author.id);
 
-    let curGodpower = godpower[message.author.id].godpower;
-    let curLevel = godpower[message.author.id].level;
+    let curGodpower = User[message.author.id].godpower;
+    let curLevel = User[message.author.id].level;
     let nextLevel = Math.floor(100*1.2**(curLevel**(4/5)));
     let newGodpower = curGodpower + godpowerAdd;
-    godpower[message.author.id].godpower = newGodpower;
-    godpower[message.author.id].total_godpower = godpower[message.author.id].total_godpower + godpowerAdd;
-    godpower[1].godpower = godpower[1].godpower + godpowerAdd;
+    User[message.author.id].godpower = newGodpower;
+    User[message.author.id].total_godpower = User[message.author.id].total_godpower + godpowerAdd;
+    totalGodpower = totalGodpower + godpowerAdd;
 
     if (nextLevel <= newGodpower) {
-        godpower[message.author.id].godpower = newGodpower - nextLevel;
-        godpower[message.author.id].level = curLevel + 1;
+        User[message.author.id].godpower = newGodpower - nextLevel;
+        User[message.author.id].level = curLevel + 1;
         let newLevel = curLevel + 2;
         let newNextLevel = Math.floor(100*1.2**((curLevel+1)**(4/5)));
         let nickname = message.guild.member(message.author) ? message.guild.member(message.author).displayName : null;
@@ -87,38 +118,43 @@ function giveGodpower(message) {
         let lvlUpEmbed = new Discord.RichEmbed()
             .setColor('d604cf')
             .setTitle(nickname+' levelled UP! <:screen_pantheonup:441043802325778442>')
-            .setDescription('You gathered '+nextLevel+' godpower <:stat_godpower:401412765232660492> and levelled up to level '+godpower[message.author.id].level+'! :tada:')
+            .setDescription('You gathered '+nextLevel+' godpower <:stat_godpower:401412765232660492> and levelled up to level '+User[message.author.id].level+'! :tada:')
             .addField("Gold rewarded", 'This isn\'t implemented yet :/')
             .setFooter(`You'll need ${newNextLevel} godpower for level ${newLevel}.`, message.author.displayAvatarURL);
-        client.channels.get(levelup_channel).send("Congratulations on reaching level "+godpower[message.author.id].level+', '+message.author+"!");
+        client.channels.get(levelup_channel).send("Congratulations on reaching level "+User[message.author.id].level+', '+message.author+"!");
         client.channels.get(levelup_channel).send(lvlUpEmbed);
     }
 
-    fs.writeFile("./godpower.json", JSON.stringify(godpower), (err) => {
-        if(err) console.log(err)
-    });
+    userData.set(User, {merge: true});
+    userData.update({1:totalGodpower});
 
     setTimeout(() => {
         godpowerCooldown.delete(message.author.id);
     }, cdSeconds * 1000);
 }
 
-function displayLevel(message) {
+async function displayLevel(message) {
 
     let user = message.mentions.users.first();
     if (!user) {
         user = message.author;
     }
 
-    if(!godpower[user.id]) {
-        godpower[user.id] = {
+    let userDoc = await userData.get();
+    let User = {};
+    if(userDoc.data()[user.id] === undefined) {
+        User[user.id] = {
             godpower: 0,
+            total_godpower: 0,
             level: 0
-        };
+        }
+        await userData.set(User, {merge: true});
+    } else {
+        User[user.id] = userDoc.data()[user.id];
     }
 
-    let curGodpower = godpower[user.id].godpower;
-    let curLevel = godpower[user.id].level;
+    let curGodpower = User[user.id].godpower;
+    let curLevel = User[user.id].level;
     let reqGodpower = Math.floor(100*1.2**(curLevel**(4/5)));
     let nextLevel = curLevel+1;
     let difference = reqGodpower - curGodpower;
