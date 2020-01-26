0361483bff967d469cf41e6716357d3782c7e876
Hedde van Heerde <heddevanheerde4@gmail.com>
2020-01-26 15:30:18 +0100

    Fixed >suggest, added base for guides command

---
+++ commands/guides.js
@@ -0,0 +1,17 @@
+/*const guides_list = [
+    ['short description', 'long description', 'url'],
+];
+
+function show_guides_list(message) {
+
+}
+
+function show_guide(message) {
+
+}
+
+function list_or_guide(message) {
+
+}
+
+exports.guides = list_or_guide;*/
