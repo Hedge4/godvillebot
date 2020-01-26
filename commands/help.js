977ebc01cb7a1d7b02cf8d28cea0d77601d0fdfc
Hedde van Heerde <heddevanheerde4@gmail.com>
2020-01-26 18:29:31 +0100

    Hopefully fixed help message for the last time

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,9 @@
 const { prefix } = require('../config.json');
 
 const commands_list = [
-    [`${prefix}command`,
-    'command_description',
-    'much longer command description'],
+    [`${prefix}example-command`,
+    'Example command description',
+    'Much longer command description'],
     [`${prefix}help`,
     'Displays this help message.'],
 ];
@@ -16,8 +16,8 @@ function constructHelp(message, Discord) {
     const helpEmbed = new Discord.RichEmbed()
         .setTitle('GodBot commands')
         .setColor(0x63CCBE) // Soft blue
-        .setDescription('GodBot gives server members XP, or \'godpower\' for talking in channels that aren\'t <#431305701021974539> or <#315874239779569666>. After reaching a specific amount of godpower, you\'ll level up.\n\u200b')
-        .addField(null, text)
+        .setDescription('GodBot gives server members XP, or \'godpower\' for talking in channels that aren\'t <#431305701021974539> or <#315874239779569666>. After reaching a specific amount of godpower, you\'ll level up.')
+        .addField('', text)
         .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/a/a4/Cute-Ball-Help-icon.png')
         .setTimestamp()
         .setFooter('GodBot is brought to you by Wawajabba', 'https://i.imgur.com/TyGn2ch.jpg');
