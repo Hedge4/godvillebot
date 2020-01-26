391a44cb5f090dbe020292872de50720a917015e
Hedde van Heerde <heddevanheerde4@gmail.com>
2020-01-26 15:36:25 +0100

    Ignoring ` charcter in suggestions

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,12 +1,14 @@
 const suggestion_channel = '670981969596645407';
 
 async function suggest(client, message) {
-    const suggestion = message.content.slice(8).trim();
+    let suggestion = message.content.slice(8).trim();
+    suggestion = suggestion.slice('`');
+    suggestion = suggestion.join();
     if (suggestion.length <= 10) {return message.reply('please add enough detail and make the description of your suggestion at least 40 characters!');}
     if (suggestion.length >= 500) {return message.reply('please be a bit more concise in your description and use less than 500 characters!');}
     const channel = await client.channels.get(suggestion_channel);
     if (channel === undefined) {message.reply('the message couldn\'t be sent.');}
-    channel.send(`${message.author.tag} sent the following suggestion from ${message.channel.name}:\n` + '`' + suggestion + '`');
+    channel.send(`${message.author.tag} sent the following suggestion from channel ${message.channel.name}:\n` + '`' + suggestion + '`');
     message.reply('thank you for your suggestion!');
 }
 
