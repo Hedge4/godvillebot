c0985eb3a5c20061f15f5409ac3421b4515268ec
Hedde van Heerde <heddevanheerde4@gmail.com>
2020-01-26 19:16:44 +0100

    moved purge to its own file

---
+++ commands/purge.js
@@ -0,0 +1,23 @@
+const { owner } = require('./config.json');
+
+
+async function purge(message) {
+    if (message.member.roles.has('313448657540349962') || message.author.id === owner) {
+        if (message.channel.id === '671046033291345943') {
+            message.delete();
+            message.channel.fetchMessages().then(messages => {
+                message.channel.bulkDelete(messages);
+                const messagesDeleted = messages.array().length; // number of messages deleted
+                // Logging the number of messages deleted on both the channel and console.
+                message.reply('deletion of messages successful. Total messages deleted: ' + messagesDeleted);
+                console.log('Deletion of messages successful. Total messages deleted: ' + messagesDeleted);
+                console.log(`Command used by ${message.author.tag}.`);
+            }).catch(err => {
+                console.log('Error while doing Bulk Delete');
+                console.log(err);
+            });
+        } else { message.reply('that command can not be used in this channel.'); }
+    } else { message.reply('you do not have access to this command.'); }
+}
+
+exports.purge = purge;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { prefix, owner, token, server, bot_id, no_xp_channels, levelup_channel, command_channels, bot_blocked } = require('./config.json');
+const { prefix, token, server, bot_id, no_xp_channels, levelup_channel, command_channels, bot_blocked } = require('./config.json');
 
 const mentions = require('./commands/togglementions');
 const giveXP = require('./commands/givexp');
@@ -10,6 +10,7 @@ const getRanking = require('./commands/ranking');
 const suggest = require('./commands/suggest');
 const guide = require('./commands/guides');
 const help = require('./commands/help');
+const purge = require('./commands/purge');
 
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json');
@@ -83,28 +84,10 @@ client.on('message', message => {
                 suggest.suggestion(client, message);
             }
             if (message.content.toLowerCase().startsWith(`${prefix}purge`)) {
-                    purge(message);
+                    purge.purge(message);
             }
         }
     }
 });
-
-async function purge(message) {
-    if (message.member.roles.has('313448657540349962') || message.author.id === owner) {
-        if (message.channel.id === '671046033291345943') {
-            message.delete();
-            message.channel.fetchMessages().then(messages => {
-                message.channel.bulkDelete(messages);
-                const messagesDeleted = messages.array().length; // number of messages deleted
-                // Logging the number of messages deleted on both the channel and console.
-                message.reply('deletion of messages successful. Total messages deleted: ' + messagesDeleted);
-                console.log('Deletion of messages successful. Total messages deleted: ' + messagesDeleted);
-            }).catch(err => {
-                console.log('Error while doing Bulk Delete');
-                console.log(err);
-            });
-        } else { message.reply('that command can not be used in this channel.'); }
-    } else { message.reply('you do not have access to this command.'); }
-}
 
 client.login(token);
