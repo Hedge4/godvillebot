a79bf16d3b831ced96a9631bf8a93d8905bd9143
Hedde van Heerde <heddevanheerde4@gmail.com>
2020-01-28 23:06:34 +0100

    Completed help message (short versions)

--- commands/godville_interaction.js
+++ commands/godville_interaction.js
@@ -1,6 +1,6 @@
 function searchWiki(message) {
     let search = message.content.slice(8).trim();
-    if (!search.length) { return message.reply('you have to input something to search the godwiki for.'); }
+    if (!search.length) { return message.reply('you have to input a search term to search the godwiki for.'); }
     search = encodeURI(search);
     return message.channel.send(`<https://wiki.godvillegame.com/index.php?search=${search}>`);
 }
