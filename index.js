9fcd6743232b38c0f0786f5517a19e2c81d2830b
Hedde van Heerde <heddevanheerde4@gmail.com>
2020-04-22 00:20:36 +0200

    Removed easter commands, added admin role colour changing gradient.

--- index.js
+++ index.js
@@ -38,7 +38,12 @@ limitedCommandsData.get()
         global.usedDaily = doc.data()['daily'];
     });
 
+    let rolkleur = 0;
+    const kleuren = ['#ff0000', '#ea0015', '#d4002b', '#bf0040', '#aa0055', '#95006a', '#800080', '#6a0095', '#5500aa', '#4000bf', '#2b00d4', '#1500ea', '#0000ff', '#0015ea', '#002bd4', '#0040bf', '#0055aa', '#006a95', '#008080', '#00956a', '#00aa55', '#00bf40', '#00d42b', '#00ea15', '#00ff00', '#2bff00', '#55ff00', '#80ff00', '#aaff00', '#d4ff00', '#ffff00', '#fff30f', '#ffe61e', '#ffda2d', '#ffcd3c', '#ffc14b', '#ffb45a', '#ffa769', '#ff9b78', '#ff8e87', '#ff8296', '#ff75a5', '#ff69b4', '#ff5896', '#ff4678', '#ff355a', '#ff233c', '#ff121e'];
+
+
 client.on('ready', () => {
+    client.guilds.get(server).me.setNickname('GodBot');
     const currentDate = new Date();
     console.log('\n' + currentDate + ` - Logged in as ${client.user.tag}!`);
     console.log('Logged in to the following guilds: ' + client.guilds.array().sort());
@@ -63,50 +68,26 @@ client.on('ready', () => {
     if (delay3 < 0) {
         delay3 = delay3 + 86400000;
     }
+
     //setTimeout(crosswordgod.dailyCrosswordRenew, delay, client);
     setTimeout(limitedCommands.reset, delay2, limitedCommandsData);
     setTimeout(crosswordgod.newsping, delay3, client);
-    client.guilds.get(server).me.setNickname('GodBot');
+    const guild = client.guilds.get(server);
+    //const adminrole = guild.roles.find(roleVal => roleVal.name === 'Moderators');
+    const adminrole = guild.roles.get(admin_role);
+    setInterval(() => {
+        if (rolkleur >= kleuren.length) {rolkleur = 0;}
+        adminrole.edit({
+            color: kleuren[rolkleur],
+        });
+        rolkleur += 1;
+    }, 8000);
 });
 
-let eggstatus = 0;
-function egg() {
-    if (eggstatus > 0) {
-        eggstatus -= 1;
-        setTimeout(egg, 1000);
-    } else {client.user.setActivity(`${prefix}help | Crossword solution updates 22:20 UTC | By Wawajabba`);}
-}
-
 client.on('message', message => {
-    if (message.content.toLowerCase().startsWith('?rank egg')) {
-        message.delete(500);
-    }
-    if (message.author.id === '155149108183695360') {
-        if (message.content.toLowerCase().includes(', you joined **egg**.')) {
-            message.delete();
-        }
-        if (message.content.toLowerCase().includes(', you left **egg**.')) {
-            message.delete();
-        }
-    }
-    if (message.channel.id === '694197607777566791') {
-        if (message.content.toLowerCase().trim() === (`${prefix}egg`)) {
-            setTimeout(() => {
-                message.react('🥚');
-                message.delete(10000);
-            }, 3000);
-
-            if (eggstatus === 0) {setTimeout(egg, 1000);}
-            eggstatus = 30;
-            client.user.setActivity(`🥚 | ${prefix}help | Crossword solution updates 22:20 UTC | By Wawajabba`);
-        }
-    }
     if (message.author.bot) {return;}
     if (bot_blocked.includes(message.author.id)) {return;}
     if (message.channel.type === 'dm') {
-        if (message.content.includes('🥚')) {
-            message.channel.send('https://youtu.be/GbNqVNWydZg');
-        }
         return console.log('A DM was sent to the bot by \'' + message.author.tag + '/' + message.author.id + '\'. The content was: \'' + message.content + '\'');
     }
 
