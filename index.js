2edf43d41becddcfc300ef1e49d5bd6ee4b89e87
Hedde van Heerde <heddevanheerde4@gmail.com>
2020-09-24 00:28:25 +0200

    Fixed syntax for previous commit

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.author.bot) {return;}
-    if (message.content.toLowerCase.startsWith('?rank') | message.content.toLowerCase.startsWith('?ranks')) {
+    if (message.content.toLowerCase().startsWith('?rank') || message.content.toLowerCase().startsWith('?ranks')) {
         if (!message.member.roles.has('313453649315495946')) {
             message.reply('use the `?ireadtherules` command to unlock core server functionality before adding any extra channels!');
         }
