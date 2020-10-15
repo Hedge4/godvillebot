12ce0149230bd90774120cb8969a3add59618f06
Hedde van Heerde <heddevanheerde4@gmail.com>
2020-10-16 01:45:00 +0200

    Minor changes to linking profiles.

--- commands/profile.js
+++ commands/profile.js
@@ -106,7 +106,7 @@ function link_profile(message, godData) {
             return message.reply('I have set or updated the link to your Godville account. Check it out with `>profile`!');
         } else {
             message.reply(`The start of your link seems correct, but '${link.slice(30)}' doesn't look like a correct god name.`);
-            return message.channel.send('God names start with a capital letter, and can only contain lowercase letters, numbers, hyphens and spaces. Using %20 to encode spaces is okay too.');
+            return message.channel.send('God names start with a capital letter, and can only contain letters, numbers, hyphens and spaces. Using %20 to encode spaces is okay too.');
         }
     } else if (/^[A-Z][a-zA-Z0-9- ]{2,29}$/.test(link)) {
         message.reply(`'${link}' looks like a god name. I have set or updated the link to your Godville account. Check it out with \`>profile\`!`);
@@ -118,7 +118,7 @@ function link_profile(message, godData) {
     } else {
         message.reply(`your link doesn't start with '<https://godvillegame.com/gods/>' or you made a typo in your god name: "${link}".`);
         message.channel.send('Please format the link exactly like this:\n\'<https://godvillegame.com/gods/YOUR-GOD-NAME>\'');
-        return message.channel.send('God names start with a capital letter, and can only contain lowercase letters, numbers, hyphens and spaces. Using %20 to encode spaces is okay too.');
+        return message.channel.send('God names start with a capital letter, and can only contain letters, numbers, hyphens and spaces. Using %20 to encode spaces is okay too.');
     }
 }
 
