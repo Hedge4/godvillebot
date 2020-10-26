8eee39988308ae1a50a82e5130f95c1a52908fa5
Hedde van Heerde <heddevanheerde4@gmail.com>
2020-10-26 03:15:06 +0100

    another bug

--- index.js
+++ index.js
@@ -86,10 +86,10 @@ client.on('message', message => {
         }
     }
     if (bot_blocked.includes(message.author.id)) {return;}
-    /*if (message.channel.type === 'dm') {
-        message.reply(`I don't currently respond to DMs. If you want such a feature to be added, contact the bot owner (Wawajabba) or use \`>suggest\` in <#${levelup_channel}>.`);
-        return console.log('A DM was sent to the bot by \'' + message.author.tag + '/' + message.author.id + '\'. The content was: \'' + message.content + '\'');
-    }*/
+    if (message.channel.type === 'dm') {
+        //message.reply(`I don't currently respond to DMs. If you want such a feature to be added, contact the bot owner (Wawajabba) or use \`>suggest\` in <#${levelup_channel}>.`);
+        console.log('A DM was sent to the bot by \'' + message.author.tag + '/' + message.author.id + '\'. The content was: \'' + message.content + '\'');
+    }
 
     if (message.guild.id === server) {
         if (message.author.id != bot_id) {
