63c8ba9b5599cc28347dc7250c0fe88a223b8c21
Hedde van Heerde <heddevanheerde4@gmail.com>
2021-11-01 04:24:00 +0100

    Made it so one zero is visible at the start of a minesweeper game

--- commands/fun/minesweeper.js
+++ commands/fun/minesweeper.js
@@ -24,43 +24,72 @@ function main(client, message) {
     const logsChannel = client.channels.cache.get(logs);
     console.log(`${message.author.tag} requested a minesweeper game in ${message.channel.name}.`);
     logsChannel.send(`${message.author.tag} requested a minesweeper game in ${message.channel.name}.`);
-    const bombs = [];
-    while (bombs.length < Math.round(size * size * bomb / 100)) {
-        const coord = `${Math.floor(Math.random() * size)}, ${Math.floor(Math.random() * size)}`;
-        if (!bombs.includes(coord)) { bombs.push(coord); }
-    }
 
+    let bombs;
+    let spoilers;
+    let indices;
     const nums = [':zero:', ':one:', ':two:', ':three:', ':four:', ':five:', ':six:', ':seven:', ':eight:'];
-    const game = [];
-    for (let x = 0; x < size; x++) {
-        game.push([]);
-        for (let y = 0; y < size; y++) {
-            let num = 0;
-            if (x - 1 >= 0 && y - 1 >= 0 && bombs.includes(`${x - 1}, ${y - 1}`)) { num++; }
-            if (y - 1 >= 0 && bombs.includes(`${x}, ${y - 1}`)) { num++; }
-            if (x + 1 <= size && y - 1 >= 0 && bombs.includes(`${x + 1}, ${y - 1}`)) { num++; }
-            if (x - 1 >= 0 && bombs.includes(`${x - 1}, ${y}`)) { num++; }
-            if (bombs.includes(`${x}, ${y}`)) { num++; }
-            if (x + 1 <= size && bombs.includes(`${x + 1}, ${y}`)) { num++; }
-            if (x - 1 >= 0 && y + 1 <= size && bombs.includes(`${x - 1}, ${y + 1}`)) { num++; }
-            if (y + 1 <= size && bombs.includes(`${x}, ${y + 1}`)) { num++; }
-            if (x + 1 <= size && y + 1 <= size && bombs.includes(`${x + 1}, ${y + 1}`)) { num++; }
+    let passes = 0;
+    const passesMax = 10;
 
-            game[x].push(nums[num]);
+    while (passes < passesMax) { // continue until a game is generated with at least one zero
+        bombs = [];
+        const game = [];
+
+        while (bombs.length < Math.round(size * size * bomb / 100)) {
+            const coord = `${Math.floor(Math.random() * size)}, ${Math.floor(Math.random() * size)}`;
+            if (!bombs.includes(coord)) { bombs.push(coord); }
         }
-    }
-    bombs.forEach(e => {
-        const xy = e.split(',');
-        game[xy[0].trim()][xy[1].trim()] = ':bomb:';
-    });
-    let spoilers = '';
-    for (let x = 0; x < size; x++) {
-        for (let y = 0; y < size; y++) {
-            spoilers += `||${game[x][y]}|| `;
+
+        for (let x = 0; x < size; x++) {
+            game.push([]);
+            for (let y = 0; y < size; y++) {
+                let num = 0;
+                if (x - 1 >= 0 && y - 1 >= 0 && bombs.includes(`${x - 1}, ${y - 1}`)) { num++; }
+                if (y - 1 >= 0 && bombs.includes(`${x}, ${y - 1}`)) { num++; }
+                if (x + 1 <= size && y - 1 >= 0 && bombs.includes(`${x + 1}, ${y - 1}`)) { num++; }
+                if (x - 1 >= 0 && bombs.includes(`${x - 1}, ${y}`)) { num++; }
+                if (bombs.includes(`${x}, ${y}`)) { num++; }
+                if (x + 1 <= size && bombs.includes(`${x + 1}, ${y}`)) { num++; }
+                if (x - 1 >= 0 && y + 1 <= size && bombs.includes(`${x - 1}, ${y + 1}`)) { num++; }
+                if (y + 1 <= size && bombs.includes(`${x}, ${y + 1}`)) { num++; }
+                if (x + 1 <= size && y + 1 <= size && bombs.includes(`${x + 1}, ${y + 1}`)) { num++; }
+
+                game[x].push(nums[num]);
+            }
         }
-        spoilers += '\n';
+        bombs.forEach(e => {
+            const xy = e.split(',');
+            game[xy[0].trim()][xy[1].trim()] = ':bomb:';
+        });
+        spoilers = '';
+        for (let x = 0; x < size; x++) {
+            for (let y = 0; y < size; y++) {
+                spoilers += `||${game[x][y]}|| `;
+            }
+            spoilers += '\n';
+        }
+
+        const regex = /||:zero:||/g;
+        let result;
+        indices = [];
+        while ((result = regex.exec(spoilers))) {
+            indices.push(result.index);
+        }
+
+        if (indices.length) break;
+        passes++;
     }
-    message.reply(`here is your minesweeper game with size ${size} and ${bombs.length} bombs:\n${spoilers}`);
+
+    if (passes < 10) {
+        const index = indices[Math.floor(Math.random() * indices.length)];
+        spoilers = spoilers.substring(0, index) + ':zero:' + spoilers.substring(index + 10);
+        message.reply(`here is your minesweeper game with size ${size} and ${bombs.length} bombs:\n${spoilers}`);
+    } else {
+        message.reply(`here is your minesweeper game with size ${size} and ${bombs.length} bombs:\n${spoilers}`);
+        message.channel.send(`After ${passesMax} attempts, none of the games I generated contained a zero. Good luck.`);
+    }
+
 }
 
 module.exports = main;
