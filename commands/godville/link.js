64a3d77e9b4c78e491f83764e5611489f6a340b9
Hedde van Heerde <heddevanheerde4@gmail.com>
2022-01-31 01:15:33 +0100

    Tidied up link.js, for logs' sake and cleaner responses

--- commands/godville/link.js
+++ commands/godville/link.js
@@ -4,33 +4,45 @@ function link_profile(message, link, godData, client) {
     if (!commandChannels.includes(message.channel.id)) {
         return message.reply(`please only use this command in <#${botvilleChannel}> to avoid spam.`);
     }
-
-    link = link.replace(/%20/g, ' ');
     const logsChannel = client.channels.cache.get(logs);
-    console.log(`${message.author.tag} tried to link their account to '${link}'.`);
-    logsChannel.send(`${message.author.tag} tried to link their account to '${link}'.`);
+
+    // we keep/make the link encoded if the user gave us a link to their page, and not the name
     if (link.startsWith('https://godvillegame.com/gods/')) {
-        if (/^[A-Z][a-zA-Z0-9- ]{2,29}$/.test(link.slice(30))) {
-            link = link.replace(/ /g, '%20');
+        link = encodeURI(link); // encodes stuff like spaces (spaces => %20)
+        const godName = decodeURI(link.slice(30)); // god name doesn't need to be encoded, also for counting characters
+        console.log(`${message.author.tag} tried to link their account to '${link}'.`);
+        logsChannel.send(`${message.author.tag} tried to link their account to '<${link}>'.`);
+
+        // test if god name meets Godville naming requirements
+        if (/^[A-Z][a-zA-Z0-9- ]{2,29}$/.test(godName)) {
             const user = {};
             user[message.author.id] = link;
             godData.set(user, { merge: true });
-            return message.reply(`I have set or updated the link to your Godville account. Check it out with \`${prefix}profile\`!`);
+            return message.reply(`I have set or updated the link to your Godville account to ${link}. Check it out with \`${prefix}profile\`!`);
         } else {
-            message.reply(`The start of your link seems correct, but '${link.slice(30)}' doesn't look like a correct god name.`);
-            return message.channel.send('God names start with a capital letter, and can only contain letters, numbers, hyphens and spaces. Using %20 to encode spaces is okay too.');
+            return message.reply(`The start of your link seems correct, but '${godName}' doesn't look like a correct god name.`
+            + ' God names start with a capital letter, and can only contain letters, numbers, hyphens and spaces. Using %20 to encode spaces is okay too.');
         }
-    } else if (/^[A-Z][a-zA-Z0-9- ]{2,29}$/.test(link)) {
-        message.reply(`'${link}' looks like a god name. I have set or updated the link to your Godville account. Check it out with \`${prefix}profile\`!`);
-        link = 'https://godvillegame.com/gods/' + link.replace(/ /g, '%20');
-        const user = {};
-        user[message.author.id] = link;
-        godData.set(user, { merge: true });
-        return;
+
+    // if user didn't send in a valid link, we decode the link because it is the god name (and we want to count characters)
     } else {
-        message.reply(`your link doesn't start with '<https://godvillegame.com/gods/>' or you made a typo in your god name: "${link}".`);
-        message.channel.send('Please format the link exactly like this:\n\'<https://godvillegame.com/gods/YOUR-GOD-NAME>\'');
-        return message.channel.send('God names start with a capital letter, and can only contain letters, numbers, hyphens and spaces. Using %20 to encode spaces is okay too.');
+        link = decodeURI(link); // this is the god name, so we don't need a separate godName variable this time
+        console.log(`${message.author.tag} tried to link their account to '${link}'.`);
+        logsChannel.send(`${message.author.tag} tried to link their account to '${link}'.`);
+
+        // test if god name meets Godville naming requirements
+        if (/^[A-Z][a-zA-Z0-9- ]{2,29}$/.test(link)) {
+            message.reply(`'${link}' looks like a god name. I have set or updated the link to your Godville account. Check it out with \`${prefix}profile\`!`);
+            link = 'https://godvillegame.com/gods/' + encodeURI(link); // if valid name we encode spaces again into %20
+            const user = {};
+            user[message.author.id] = link;
+            godData.set(user, { merge: true });
+            return;
+        } else {
+            return message.reply(`your link doesn't start with '<https://godvillegame.com/gods/>' or you made a typo in your god name: "${link}".`
+            + '\nPlease format the link exactly like this:\n\'<https://godvillegame.com/gods/YOUR-GOD-NAME>\''
+            + '\n\nGod names start with a capital letter, and can only contain letters, numbers, hyphens and spaces. Using %20 to encode spaces is okay too.');
+        }
     }
 }
 
