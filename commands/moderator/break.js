c518e140bd9a05c5b3f7411449cbc4f564243d80
Hedde van Heerde <heddevanheerde4@gmail.com>
2022-10-15 02:14:48 +0200

    Improved logging for >break

--- commands/moderator/break.js
+++ commands/moderator/break.js
@@ -1,19 +1,23 @@
 const { token, channels } = require('../../configurations/config.json');
+const logger = require('../features/logging');
 
 async function pauseBot(message, client) {
-    let args = message.content.slice(6).trim().split(' ')[0];
-    if (!args) { args = 1; }
-    if (isNaN(args)) { return (message.reply('Please specify a number of minutes that the bot should pause for.')); }
-    args = Math.ceil(args);
-    if (args < 1 || args > 60) { return message.reply('The number of minutes should be in the range 1-60.'); }
-    await message.channel.send(`Got it. I will be offline for ${args} minutes, or until the hosting service resets.`);
-    await client.channels.cache.get(channels.modLogs).send(`${message.author.tag} paused the bot for ${args} ${quantiseWords(args, 'minute')}.`);
+    let mins = message.content.slice(6).trim().split(' ')[0];
+    if (!mins) { mins = 1; }
+    if (isNaN(mins)) { return (message.reply('Please specify a number of minutes that the bot should pause for.')); }
+    mins = Math.ceil(mins);
+    if (mins < 1 || mins > 60) { return message.reply('The number of minutes should be in the range 1-60.'); }
+
+    logger.log(`${message.author.tag} paused the bot for ${mins} ${quantiseWords(mins, 'minute')}.`); // add await here?
+    await message.channel.send(`Got it. I will be offline for ${mins} minutes, or until my server restarts.`);
+    await client.channels.cache.get(channels.modLogs).send(`${message.author.tag} paused the bot for ${mins} ${quantiseWords(mins, 'minute')}. Links: <${message.url}>`);
     await client.destroy();
-    setTimeout(resumeBot, args * 60000, client, message);
+    setTimeout(resumeBot, mins * 60000, client, message, mins);
 }
 
-async function resumeBot(client, message) {
+async function resumeBot(client, message, mins) {
     await client.login(token);
+    logger.log(`Bot is back online after waiting ${mins} minutes.`);
     message.channel.send('Back online!');
 }
 
