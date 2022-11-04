b9ae31a3c4f6075a17394df0fe3b508b9b22a490
Hedde van Heerde <heddevanheerde4@gmail.com>
2022-11-04 18:35:40 +0100

    Fixed too frequent omnibus list update
    Accidentally updated every 1440 seconds, not minutes,
    which is every 24 minutes instead of the intended once a day.

--- commands/crosswordgod/omnibusManager.js
+++ commands/crosswordgod/omnibusManager.js
@@ -93,7 +93,7 @@ async function loadOmnibus() {
     // schedule new automatic update
     nextUpdateTimer = setTimeout(function() {
         omnibusStartup(true);
-    }, autoUpdateDelay * 1000);
+    }, autoUpdateDelay * 60 * 1000);
     logger.log(`Omnibus: Scheduled next automatic update ${autoUpdateDelay} minutes from now.`);
 
     const html = await downloadOmnibus();
