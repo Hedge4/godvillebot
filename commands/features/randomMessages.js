790f673e5e2dad88ba58fc73a16e8c74e048d242
Hedde van Heerde <heddevanheerde4@gmail.com>
2023-10-02 21:23:15 +0200

    Added a new randomMessageEvent, GodBotIsWatching
    Bot now rarely sends 👀 and deletes it after a few seconds

--- commands/features/randomMessages.js
+++ commands/features/randomMessages.js
@@ -1,7 +1,6 @@
 // triggers the bot reacts to, and their possible reactions
-const messageEvents = [
-    {
-        name: 'GodvilleHalloween',
+const messageEvents = {
+    GodvilleHalloween: {
         active() { return (new Date).getMonth() === 9; }, // only in October for Halloween
         minDelay: 0.5 * (60 * 1000), // in minutes
         maxDelay: 15 * (60 * 1000),
@@ -11,27 +10,48 @@ const messageEvents = [
         messages: [
             'https://cdn.discordapp.com/attachments/353554102833250305/1025469102912966756/IMG_20220930_140327.jpg',
         ],
+    }, GodBotIsWatching: {
+        active() { return true; },
+        minDelay: 0.5 * (60 * 60 * 1000), // in hours
+        maxDelay: 18 * (60 * 60 * 1000),
+        autoDelete: true,
+        deleteAfter: 4 * 1000,
+        channelId: '313398424911347712',
+        messages: [
+            'https://cdn.discordapp.com/attachments/353554102833250305/1025469102912966756/IMG_20220930_140327.jpg',
+        ],
     },
-];
+};
 
 
 // executed once on bot startup
 async function setup(client) {
     // set a timer for each messageEvent according to the stored delays
-    messageEvents.forEach(async e => {
-        if (!e.active()) return;
-
+    Object.values(messageEvents).forEach(async e => {
         const channel = await client.channels.fetch(e.channelId);
-        const timeout = e.minDelay + Math.round(Math.random() * e.maxDelay);
+        const timeout = random(e.minDelay, e.maxDelay);
 
         setTimeout(() => {
             sendMessage(e, channel);
-        }, timeout);
+            // if not active, schedule next check for 12 hours later
+        }, e.active() ? timeout : 12 * 60 * 60 * 1000);
     });
 }
 
 // triggers when the delay has elapsed
 function sendMessage(messageEvent, channel) {
+    // schedule new messageEvent immediately
+    const timeout = random(messageEvent.minDelay, messageEvent.maxDelay);
+
+    setTimeout(() => {
+        sendMessage(messageEvent, channel);
+        // if not active, schedule next check for 12 hours later
+    }, messageEvent.active() ? timeout : 12 * 60 * 60 * 1000);
+
+    // return if the messageEvent is currently not active
+    if (!messageEvent.active()) return;
+
+    // if active and not returned, send a random message
     channel.send(messageEvent.messages[Math.floor(Math.random() * messageEvent.messages.length)]).then(msg => {
         if (messageEvent.autoDelete) {
             setTimeout(() => {
@@ -39,13 +59,8 @@ function sendMessage(messageEvent, channel) {
             }, messageEvent.deleteAfter);
         }
     });
-
-    // schedule new messageEvent
-    const timeout = messageEvent.minDelay + Math.round(Math.random() * messageEvent.maxDelay);
-
-    setTimeout(() => {
-        sendMessage(messageEvent, channel);
-    }, timeout);
 }
+
+const random = (min, max) => min + Math.round(Math.random() * (max - min));
 
 module.exports = setup;
