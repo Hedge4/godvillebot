a7b7f5b472433894479ea0e1726230545bddba70
Hedde van Heerde <heddevanheerde4@gmail.com>
2024-10-04 08:07:51 +0200

    update >code command to reflect now public repository

--- commands/useful/code.js
+++ commands/useful/code.js
@@ -1,5 +1,10 @@
+const { repository: { url: repositoryURL } } = require('../../package.json');
+
 async function main(message) {
-    return message.reply('My code is not public yet, but the bot creator (Wawajabba#6917) plans to make the GitHub repository fully public in the future!');
+    return message.reply('The bot\'s creator (@wawajabba) has made the GitHub repository public!'
+        + `\nYou're free to check out the code at ${repositoryURL}.`
+        + '\n\nYou can also use parts of the code, but please remember to read LICENSE.md before you do.'
+        + '\nContributions are very welcome! Contact me if you have any ideas or need help to get started, or just submit a pull request.');
 }
 
 module.exports = main;
